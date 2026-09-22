30ed4e497802506996d4bf8141a4c55f6b0b8088
Karol Ximena Sanchez Avila <kxsanchez2017@gmail.com>
2026-09-22 17:04:40 -0500

    Delete Frontend

--- Frontend_Backend_Caja/LenteMagico-C/Frontend/src/Componentes/FooterC.jsx
+++
@@ -1,8 +0,0 @@
-function FooterC() {
-  return (
-    <footer>
-      <p>© 2026 Lente Magico</p>
-    </footer>
-  )
-}
-export default FooterC;
--- Frontend_Backend_Caja/LenteMagico-C/Frontend/src/Componentes/HeaderC.jsx
+++
@@ -1,8 +0,0 @@
-function HeaderC() {
-  return (
-   <header>
-        <h3>LENTE MAGICO</h3>
-    </header>
-  )
-}
-export default HeaderC;
